f0f3f8ab2fc4222a4c30995a5f77829c835813be
GajyShankar <gajyshankar.925006@gmail.com>
2026-04-05 22:46:19 +0530

    fix: remove accelerate for neon db

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,9 @@
 import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
 
-const globalForPrisma = global as unknown as { prisma: any }
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient().$extends(withAccelerate())
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
